06eb76289ea27934610fb090c5563f0c31e2ca77
Thullyoo <thullyofilho@gmail.com>
2025-01-21 13:31:41 -0300

    Initial commit

---
+++ src/main/java/com/thullyoo/security_keycloack/SecurityKeycloackApplication.java
@@ -0,0 +1,13 @@
+package com.thullyoo.security_keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityKeycloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityKeycloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyoo/security_keycloack/SecurityKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyoo.security_keycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
